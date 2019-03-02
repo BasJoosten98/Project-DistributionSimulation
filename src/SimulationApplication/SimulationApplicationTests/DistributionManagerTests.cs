f1bd48e429f9fb8e8116e6787b7b2169b07c0da4
Miguel Boekhold <m.boekhold@student.fontys.nl>
2019-03-02 15:38:10 +0100

    Unit testing packages and testing framework

---
+++ src/SimulationApplication/SimulationApplicationTests/DistributionManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using SimulationApplication.ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationApplication.ClassLibrary.Tests
+{
+    public class DistributionManagerTests
+    {
+        [Fact()]
+        public void CalculatePathTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
