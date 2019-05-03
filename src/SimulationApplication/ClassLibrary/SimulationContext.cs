33577b10a973471b710e27edc9159366e379f593
Remy Bos <i231896@fhict.nl>
2019-05-03 12:29:44 +0200

    Add packages and initial Context setup

---
+++ src/SimulationApplication/ClassLibrary/SimulationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary
+{
+    class SimulationContext : DbContext
+    {
+        public DbSet<Map> Maps { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var envVariable = ConfigurationManager.AppSettings["envVariableName"];
+                string connectionString = Environment.GetEnvironmentVariable(envVariable);
+                optionsBuilder.UseMySql(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
