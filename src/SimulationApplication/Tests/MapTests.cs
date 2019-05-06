49ec50cc5243490ada3174d11f340a7f8fdd7118
Remy Bos <i231896@fhict.nl>
2019-05-06 15:29:29 +0200

    Add test case for testing the map's functionality. In this case a test for adding a road to the collection of edges.

--- src/SimulationApplication/Tests/MapTests.cs
+++ src/SimulationApplication/Tests/MapTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using Xunit;
 
 namespace ClassLibrary.Tests
@@ -20,6 +21,8 @@ namespace ClassLibrary.Tests
         [InlineData(4, 0)]
         // Should result in 0 locations in the map's collection of locations as the number of cells should be set to a 0x0 grid.
         [InlineData(4, -13)]
+        // Should result in 0 locations in the map's collection of locations as the number of locations is < 0.
+        [InlineData(-10, -13)]
         public void TestNumberOfLocations(int numberOfLocations, int numberOfCells)
         {
             const int CELLSIZE = 40;
@@ -49,6 +52,44 @@ namespace ClassLibrary.Tests
                     Assert.True(locationCount == numberOfCells * numberOfCells);
                 }
             }
+        }
+
+        [Theory]
+        // Should throw an ArgumentOutOfRangeException
+        [InlineData(-1, 0)]
+        // Should throw an ArgumentOutOfRangeException
+        [InlineData(-1, -1)]
+        // Should throw an ArgumentOutOfRangeException
+        [InlineData(3, -100)]
+        // Add a road between the Location itself, should result in location 0.
+        [InlineData(0, 0)]
+        // Add a road between the two Locations should result in a road where Road.vertex1 == map.Locations[1] and Road.vertex2 == map.Locations[2].
+        [InlineData(1, 2)]
+        // Add a road between the two Locations should result in a road where Road.vertex1 == map.Locations[3] and Road.vertex2 == map.Locations[1].
+        [InlineData(3, 1)]
+        // Should throw an ArgumentOutOfRangeException, since the number of locations is less than 12.
+        [InlineData(1, 12)]
+        public void TestCreateEdge(int source, int destination)
+        {
+            const int NUMBER_OF_LOCATIONS = 4;
+            const int NUMBER_OF_CELLS = 4;
+            const int CELLSIZE = 40;
+            PictureBox EMPTY_PICTURE_BOX = new PictureBox();
+
+            Map map = new Map(NUMBER_OF_LOCATIONS, NUMBER_OF_CELLS, CELLSIZE, EMPTY_PICTURE_BOX);
+
+            if (source < 0 || destination < 0 || source > map.Locations.Count - 1 || destination > map.Locations.Count - 1)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => map.Edges.Add(new Road(map.Locations[source], map.Locations[destination])));
+            }
+            else
+            {
+                map.Edges.Add(new Road(map.Locations[source], map.Locations[destination]));
+                Assert.True(map.Edges.Count == 1);
+                Road edge = map.Edges[0]; // As the collection should only hold 1 edge.
+                Assert.True(edge.Vertex1 == map.Locations[source] && edge.Vertex2 == map.Locations[destination]);
+            }
+
         }
     }
 }
