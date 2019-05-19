666ffc20976b7801fb280d20bf12e8e4f943015c
Miguel Boekhold <m.boekhold@student.fontys.nl>
2019-05-19 21:05:24 +0200

    Edited warehousetests

--- src/SimulationApplication/Tests/WarehouseTests.cs
+++ src/SimulationApplication/Tests/WarehouseTests.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using Xunit;
+using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimulationApplicationTests
+namespace ClassLibrary.Tests
 {
-    class WarehouseTests
+    public class WarehouseTests
     {
+        [Fact()]
+        public void RemoveAllvehiclesTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void WarehouseTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddVehicleTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void fastestVehicleAvailableTimeTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetShopTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void NextTickTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
     }
 }
