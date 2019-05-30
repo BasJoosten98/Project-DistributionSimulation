3af8d8c010c69f70100f7a00f9421910fe44a2d8
Miguel Boekhold <m.boekhold@student.fontys.nl>
2019-05-30 22:58:33 +0200

    Added tests for best placements

---
+++ src/SimulationApplication/Tests/BestPlacementTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    public class BestPlacementTests
+    {
+        [Fact()]
+        public void BestPlacementTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void MakeBackupsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CheckBestPlacementTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CheckCombinationsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetBestBuildTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetBuildingsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
